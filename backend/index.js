04f9bd0adb3826f67161bcd5efb7d686adf3d1cc
Ashish48Maurya <maurya.48.ashish.com>
2024-03-13 19:22:47 +0530

    Added Backend Files

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const router = require("./routers/routes");
 const bodyParser = require('body-parser')
-// const Admin = require('./models/Admin');
 const mongoConnect = require('./db')
 const PORT = 8000;
 const cors = require("cors");
@@ -15,15 +14,6 @@ app.use(require("./routers/routes"));
 app.use(bodyParser.json());
 
 mongoConnect(process.env.MONGO_URL).then(async () => {
-
-  // const newAdmin = new Admin();
-  // try {
-  //   await newAdmin.save();
-  //   console.log('Admin saved to the database');
-  // } catch (error) {
-  //   console.error('Error saving admin:', error);
-  // }
-
   app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
   });

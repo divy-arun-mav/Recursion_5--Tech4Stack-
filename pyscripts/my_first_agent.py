7ff6d74004c489eee28bb6988d92d5eaa3f03184
Divy Arun Mav <113162746+divy-arun-mav@users.noreply.github.com>
2024-03-22 15:53:24 +0530

    updating changes

--- pyscripts/my_first_agent.py
+++
@@ -1,8 +0,0 @@
-import cosmpy
-from cosmpy.aerial.client import LedgerClient, NetworkConfig
- 
-ledger_client = LedgerClient(NetworkConfig.fetch_mainnet())
- 
-address: str = 'fetch12q5gw9l9d0yyq2th77x6pjsesczpsly8h5089x'
-balances = ledger_client.query_bank_all_balances(address)
-print(balances)
